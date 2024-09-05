2504b4477fe1915575652e907d3656cada9c184a
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-09-05 17:07:48 +0200

    esercizio terminato

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-console.log('ciao')
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      todoList:[
-        {
-            text:'fare le prove col gruppo',
-            done: true
-        },
-        {
-            text:'riunione condominiale',
-            done: false
-        },
-        {
-            text:'fare esercizi di Boolean',
-            done: true
-        },
-        {
-            text:'uscire con gli amici',
-            done: true
-        },
-        {
-            text:'andare al  concerto dei Pixies',
-            done: true
-        },
-      ]
+      todoList: [
+        { text: 'fare le prove col gruppo', done: true },
+        { text: 'riunione condominiale', done: false },
+        { text: 'fare esercizi di Boolean', done: true },
+        { text: 'uscire con gli amici', done: true },
+        { text: 'andare al concerto dei Pixies', done: true }
+      ],
+      newTodoText: ''
     };
-
   },
   methods: {
     removeTodo(index) {
       this.todoList.splice(index, 1);
+    },
+    addTodo() {
+      if (this.newTodoText.trim()) {
+        this.todoList.push({ text: this.newTodoText, done: false });
+        this.newTodoText = '';
+      }
     }
   }
-}).mount('#app')
+}).mount('#app');
