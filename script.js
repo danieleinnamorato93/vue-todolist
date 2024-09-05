5c6c62da1b00b5ade74370be0f8338e5cdf5634a
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-09-05 16:03:39 +0200

    milestoonne#1

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
+console.log('ciao')
 const { createApp } = Vue
 
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
-    }
+      todoList:[
+        {
+            text:'fare le prove col gruppo',
+            done: true
+        },
+        {
+            text:'riunione condominiale',
+            done: false
+        },
+        {
+            text:'fare esercizi di Boolean',
+            done: true
+        },
+        {
+            text:'uscire con gli amici',
+            done: true
+        },
+        {
+            text:'andare al  concerto dei Pixies',
+            done: true
+        },
+      ]
+    };
   }
 }).mount('#app')
